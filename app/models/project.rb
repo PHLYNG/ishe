18312d4f3272562da34c435a8aa0761691a9400d
Dave Corderman <dcordz@gmail.com>
2016-08-07 09:09:17 -0400

    deleted team projects branch and then remade from padawan

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,8 +5,7 @@ class Project < ApplicationRecord
   has_many :user_join_projects, dependent: :destroy
   has_many :users, through: :user_join_projects
 
-  before_create :check_project_exists
-  after_create :create_new_project
+  before_create :create_new_project
 
   def english_date
     database_date = self.project_action_date
@@ -20,19 +19,42 @@ class Project < ApplicationRecord
   #   end
   # end
 
-  def check_project_exists
+  def check_project_exists(proj)
     # for merging two or more projects together
-    if Project.exists?(project_type: "Pothole", street1: "C St.", street2: "D St.")
+    if self == Project.where( project_type: "Pothole",
+                      street1: "C St.",
+                      street2: "D St." ).exists?   ||
+
+        self == Project.where(project_type: "Pothole",
+                      street1: "D St.",
+                      street2: "C St." ).exists?
+      binding.pry
       return self
     end
   end
   # binding.pry
   def create_new_project
-    if check_project_exists == true
-      proj = Project.create(:project_type => "Pothole", :street1 => "C St.", :street2 => "D St.")
-      Team.create
+    debugger
+    if check_project_exists(self) == true
 
+      # new_proj =
+      Project.create(
+                  project_type: check_project_exists.project_type,
+                  street1: check_project_exists.street1,
+                  street2: "E St.")
+                  # street2: check_project_exists.street2 )
+
+      # Team.create(project_id: new_proj.id,
+                  # users: [current_user, get_users_for_new_team )
     end
+  end
+
+  def get_users_for_new_team
+    users_for_team = []
+    check_project_exists.users.each do |user|
+      users_for_team.push(user)
+    end
+    return users_for_team
   end
 
   # before create, check if project exists already.
