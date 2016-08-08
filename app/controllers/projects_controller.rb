a15fe2094e5da7adf3b6e786f8022cda8c2dfc92
Dave Corderman <dcordz@gmail.com>
2016-08-08 11:52:26 -0400

    need to check out something grid for mailing, rendering projects to json

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
     # if project doesn't exist create project, if it does take user to that project
 
     # creating with dup_project_params, which doesn't include action date and complete? Problem?
-    @project = Project.find_or_create_by(dup_project_params)
+    @project = Project.find_or_create_by(dup_project_params.merge({project_action_date: Time.new()+(7*60*60*24)}))
 
     # UserJoinProject does not contain User
     check_user_proj_has_user = 0
@@ -35,14 +35,14 @@ class ProjectsController < ApplicationController
       end
 
       # if number of users before save is == 4, new user will be number five, therefore set action date to +1 week after user joins project
-      if UserJoinProject.where(project_id: @project.id).count == 4
-        @project.project_action_date = Time.new()+(7*60*60*24)
-      end
+      # if UserJoinProject.where(project_id: @project.id).count == 4
+      #   @project.project_action_date = Time.new()+(7*60*60*24)
+      # end
 
       # set project complete
-      if Time.now() > UserJoinProject.where(project_id: @project.id).project_action_date
-        @project.project_complete == true
-      end
+      # if Time.now() > UserJoinProject.where(project_id: @project.id).project_action_date
+      #   @project.project_complete == true
+      # end
 
     else
       UserJoinProject.create!({user: current_user, project: @project})
@@ -54,6 +54,10 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @project, status: :created, location: @project }
+    end
     @user = current_user
   end
 
@@ -61,10 +65,10 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    # proj = Project.find(params[:id])
-    # ujp = UserJoinProject.where(project_id: proj.id)
-    # ujp.delete_all
-    # proj.delete
+    proj = Project.find(params[:id])
+    ujp = UserJoinProject.where(project_id: proj.id, user_id: current_user.id)
+    ujp.delete_all
+    proj.delete
     redirect_to current_user
   end
 
@@ -75,7 +79,7 @@ class ProjectsController < ApplicationController
   # end
 
   def dup_project_params
-    params.require(:project).permit(:project_type, :street1, :street2)
+    params.require(:project).permit(:project_type, :street1, :street2, :project_action_date)
   end
 
   # def user_join_project_params
