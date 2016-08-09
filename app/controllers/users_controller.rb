fb19d88d13b50229d3a616c710322d55338ceec3
Dave Corderman <dcordz@gmail.com>
2016-08-09 08:22:33 -0400

    deleted some mailer stuff except the views and added sendgrid gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,37 +7,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     # if user is valid
-
-
-    respond_to do |format|
-      if @user.save
-        # Tell the UserMailer to send a welcome email after save
-        log_in(@user)
-        uservar = UserMailer.welcome_email(@user).deliver_now
-        binding.pry
-        format.html { redirect_to(@user, notice: 'Welcome to Ishe, ready to get to work?') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-
-
-
-    # if @user.save
-      # Tell the ApplicationMailer to send a welcome email after save
-      # UserMailer.welcome_email(@user).deliver_now
+    if @user.save
       # log in that user
-      # log_in @user
+      log_in @user
       # flash message welcome
-      # flash[:success] = "Welcome to Ishe, ready to get to work?"
+      flash[:success] = "Welcome to Ishe, ready to get to work?"
       # redirect to user page
-      # redirect_to @user
-    # else
+      redirect_to @user
+    else
       # if user save fails, render the new user registration page
-      # render 'new'
-    # end
+      render 'new'
+    end
   end
 
   def show
--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'from@example.com'
-  def welcome_email(user)
-      @user = user
-      @url  = 'http://example.com/Login'
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-   end
-end
