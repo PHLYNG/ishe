dcaa20a9fe887e6da243361d1e6461ee85a1a7f7
Dave Corderman <dcordz@gmail.com>
2016-08-11 20:42:32 -0400

    added email to prod environment'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ishe_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'https://mysterious-sea-27623.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
