049ad4eceb289d20e2973386906709d7c1e8ca59
Dave Corderman <dcordz@gmail.com>
2016-09-21 12:46:11 -0400

    updated project model to reflect gps similarities

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,52 +13,68 @@ class Project < ApplicationRecord
   :path => ":rails_root/public/assets/projects/:id/:style/:basename.:extension"
 
   # rubular.com
-  VALID_STREET_REGEX = /\A\d{0,6}([s][t]|[t][h]|[r][d]){0,2}\s{0,1}[a-zA-Z]{1,50}.{0,1}\s{1}[a-zA-Z]{0,50}.{0,1}\s{0,1}[a-zA-Z]{0,50}.{0,1}[a-zA-Z]{2}.{0,1}$\z/
+  # VALID_STREET_REGEX = /\A\d{0,6}([s][t]|[t][h]|[r][d]){0,2}\s{0,1}[a-zA-Z]{1,50}.{0,1}\s{1}[a-zA-Z]{0,50}.{0,1}\s{0,1}[a-zA-Z]{0,50}.{0,1}[a-zA-Z]{2}.{0,1}$\z/
 
   # VALID_CITY_REGEX = /\A[a-zA-Z][a-z]{3,20}(\s?|-?)[a-z]{0,20}(\s?|-?)[a-z]{0,20}(\s?|-?)[a-z]{0,20}\z/
-  VALID_CITY_REGEX = /\A[a-zA-Z]+(?:(?:\s+|-)[a-zA-Z]+)*\z/
+
+  # used this one not needed with gps
+  # VALID_CITY_REGEX = /\A[a-zA-Z]+(?:(?:\s+|-)[a-zA-Z]+)*\z/
 
   validates :project_type, presence: true
 
   # validate streets presence and length
   # downcase street names to ensure matching
-  validates :street1, presence: true, length: { minimum: 5 }, format: { with: VALID_STREET_REGEX }
-  validates :street2, presence: true, length: { minimum: 5 }, format: { with: VALID_STREET_REGEX }
-  validates :state, presence: true
-  validates :city, presence: true, format: { with: VALID_CITY_REGEX }
+  validates :street1, presence: true, length: { minimum: 5 }#, format: { with: VALID_STREET_REGEX }
+  validates :street2, presence: true, length: { minimum: 5 }#, format: { with: VALID_STREET_REGEX }
+  # validates :state, presence: true
+  # validates :city, presence: true, format: { with: VALID_CITY_REGEX }
 
   # if streets aren't different, new proj page will be rendered again, cross streets can't be the same street
-  def streets_are_not_different
-    self.street1.downcase
-    self.street2.downcase
-    if (self.street1).similar(self.street2) >= 80.0
-      return true
+  # def streets_are_not_different
+  #   self.street1.downcase
+  #   self.street2.downcase
+  #   if (self.street1).similar(self.street2) >= 80.0
+  #     return true
+  #   end
+  # end
+
+  # GPS version -  Returns an array of projects that are similar if project exists
+  def project_exists
+    if self.check_project_exists == true
+      return (Project.select{ |proj| proj.street1.between?((self.street1 + 0.0002), (self.street1 - 0.0002))}.count > 0 && Project.select{ |proj| proj.street2.between?((self.street2 + 0.0002), (self.street2 - 0.0002))}.count > 0)
     end
+  end
+
+  # GPS version - check for projects that have similar streets reversing street order as necessary.
+
+  def check_project_exists
+    # if latitude is within +/- 2 ten-thounsandths of another project's latitude it is the same
+    if (Project.select{ |proj| proj.street1.between?((self.street1 + 0.0002), (self.street1 - 0.0002))}.count > 0 && Project.select{ |proj| proj.street2.between?((self.street2 + 0.0002), (self.street2 - 0.0002))}.count > 0)
   end
 
   # Returns an array of projects that are similar if project exists
-    def project_exists
-
-      if self.check_project_exists == true
-        return (Project.select{ |proj| proj.street1.similar(self.street1 || self.street2) >= 85.0 }) &&
-        (Project.select{ |proj| proj.street2.similar(self.street2 || self.street1) >= 85.0 }) &&
-        (Project.select{ |proj| proj.city.similar(self.city) >= 95.0 }) &&
-        (Project.select{ |proj| proj.state == self.state }) &&
-        (Project.select{ |proj| proj.project_type == self.project_type })
-      else
-        return []
-      end
-    end
-
-  # check for projects that have similar streets reversing street order as necessary.
-  # checks if project exists and returns true or false
-  def check_project_exists
-    ((Project.select{ |proj| proj.street1.similar(self.street1) >= 90.0 }.count > 0) || (Project.select{ |proj| proj.street1.similar(self.street2) >= 90.0 }.count > 0)) &&
-    ((Project.select{ |proj| proj.street2.similar(self.street1) >= 90.0 }.count > 0) || (Project.select{ |proj| proj.street1.similar(self.street2) >= 90.0 }.count > 0)) &&
-    (Project.select{ |proj| proj.city.similar(self.city) >= 95.0 }.count > 0) &&
-    (Project.select{ |proj| proj.state == self.state }.count > 0) &&
-    (Project.select{ |proj| proj.project_type == self.project_type }.count > 0)
-  end
+  #   def project_exists
+  #
+  #     if self.check_project_exists == true
+  #       return (Project.select{ |proj| proj.street1.similar(self.street1 || self.street2) >= 85.0 }) &&
+  #       (Project.select{ |proj| proj.street2.similar(self.street2 || self.street1) >= 85.0 }) &&
+  #       (Project.select{ |proj| proj.city.similar(self.city) >= 95.0 }) &&
+  #       (Project.select{ |proj| proj.state == self.state }) &&
+  #       (Project.select{ |proj| proj.project_type == self.project_type })
+  #     else
+  #       return []
+  #     end
+  #   end
+  #
+  # # check for projects that have similar streets reversing street order as necessary.
+  # # checks if project exists and returns true or false
+  # def check_project_exists
+  #   ((Project.select{ |proj| proj.street1.similar(self.street1) >= 90.0 }.count > 0) || (Project.select{ |proj| proj.street1.similar(self.street2) >= 90.0 }.count > 0)) &&
+  #   ((Project.select{ |proj| proj.street2.similar(self.street1) >= 90.0 }.count > 0) || (Project.select{ |proj| proj.street1.similar(self.street2) >= 90.0 }.count > 0)) &&
+  #   (Project.select{ |proj| proj.city.similar(self.city) >= 95.0 }.count > 0) &&
+  #   (Project.select{ |proj| proj.state == self.state }.count > 0) &&
+  #   (Project.select{ |proj| proj.project_type == self.project_type }.count > 0)
+  # end
 
   # Validate filename
   validates :photo, attachment_presence: true
