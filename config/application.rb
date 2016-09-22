3fcd320d082032f8fa1e25bf93cc9dc478877973
Dave Corderman <dcordz@gmail.com>
2016-09-21 22:27:54 -0400

    reverse geocoding is not easy

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,14 @@ module Ishe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+   # Rails 5 Cors
+   # 
+  #  config.middleware.insert_before 0, Rack::Cors do
+  #    allow do
+  #      origins '*'
+  #      resource '*', :headers => :any, :methods => [:get, :post, :options]
+  #    end
+  #  end
   end
 end
