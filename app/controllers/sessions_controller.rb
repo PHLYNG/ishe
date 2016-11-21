2e45d5d5d0fd5a787387a8ae17d3ff85910fe82a
Dave Corderman <dcordz@gmail.com>
2016-11-21 12:45:17 -0500

    reimplement local user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,27 +11,31 @@ class SessionsController < ApplicationController
   end
 
   def create
-    begin
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = @user.id
-      flash[:success] = "Welcome, #{@user.name}!"
-      redirect_to @user
-    rescue
-      binding.pry
-      flash[:warning] = "There was an error while trying to authenticate you..."
-      redirect_to root_url
-    end
-    #
-    # user = User.find_by(email: params[:session][:email].downcase)
-    #
-    # if user && user.authenticate(params[:session][:password])
-    #   # call log_in helper method and pass user as argument
-    #   log_in user
-    #   redirect_to user
-    # else
-    #   flash.now[:danger] = "Error loggin in, please check your username and password and try again."
-    #
-    #   render 'new'
+    if params[:provider]
+      begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome, #{@user.name}!"
+        redirect_to @user
+      rescue
+        binding.pry
+        flash[:warning] = "There was an error while trying to authenticate you..."
+        redirect_to root_url
+      end
+
+    else
+      user = User.find_by(email: params[:session][:email].downcase)
+
+      if user && user.authenticate(params[:session][:password])
+        # call log_in helper method and pass user as argument
+        log_in user
+        redirect_to user
+      else
+        flash.now[:danger] = "Error loggin in, please check your username and password and try again."
+        render 'new'
+      end #end local login else
+      
+    end #end if [:provider]
     #
     #   # the above is incorrect without the ".now" because re-rendering a template with render doesn’t count as a request, and flash persist across the site-layout until a new request is made
   end
